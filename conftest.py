81107545d0534983aed4eafa96585733e2d96988
Jacob Ragan <j2berntal@gmail.com>
2026-04-26 15:21:41 +0500

    2. Added Exercise 2, Updated README and new Test Cases

--- conftest.py
+++ conftest.py
@@ -14,13 +14,4 @@ def driver():
         driver = webdriver.Chrome(options=chrome_options)
     driver.get('https://automationteststore.com/')
     yield driver
-    # sleep(3)
-    # wait = WebDriverWait(driver, 8)
-    # try:
-    #     alert = wait.until(EC.alert_is_present())
-    #     text = alert.text
-    #     assert text == "Message received!"
-    #     alert.accept()
-    # except TimeoutException:
-    #     pytest.fail("Поле Name не заполнено или submit не сработал. Форма не отправилась")
     driver.quit()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 from pages.sorted_page import SortedPage
-
+from pages.cart_page import CartPage
+from time import sleep
 
 def test_sorting_by_ascending_prices(driver):
     sorted_page = SortedPage(driver)
@@ -59,3 +60,34 @@ def test_sorting_by_descending_names(driver):
     print("\nZ - A. Actual Names: ", names_actual)
 
     assert names_actual == expected_names
+
+
+def test_adding_products_to_cart(driver):
+    cart_page = CartPage(driver)
+    sorted_page = SortedPage(driver)
+
+    cart_page.find_shirts()
+    sorted_page.sort_by_name_a_to_z()
+    added_products = cart_page.add_second_and_third_to_cart()
+    print("\nInitial Data of products: ", added_products)
+    actual_products = cart_page.get_data_from_cart()
+    print("\nActual Data from products: ", actual_products)
+    assert actual_products == added_products
+
+
+def test_total_price(driver):
+    cart_page = CartPage(driver)
+    sorted_page = SortedPage(driver)
+    cart_page.find_shirts()
+    sorted_page.sort_by_name_a_to_z()
+    cart_page.add_second_and_third_to_cart()
+
+    cart_page.double_cheapest_price()
+    calculated_total = round(cart_page.calculate_expected_price(), 2)
+    cart_ui_total = cart_page.get_ui_total()
+    print("\nTest №2:")
+    print("\nCalculated Total: ", calculated_total, "$")
+    print("\nUI Total in Cart: ", cart_ui_total, "$")
+    assert calculated_total == cart_ui_total
+
+    sleep(0.5)
